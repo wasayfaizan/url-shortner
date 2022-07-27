71222a0c0eaa14ec12f227a2f3565eca2ba547bc
Wasay Faizan <a.wasayfaizan@hotmail.com>
2022-07-27 23:09:02 +0500

    initial commits

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
+mongoose.connect('mongodb://localhost/urlshortner', {
+    useNewUrlParser : true, 
+    useUnifiedTopology : true
+})
 
 
 app.set('view engine', 'ejs')
@@ -9,5 +14,10 @@ app.set('view engine', 'ejs')
 app.get('/', (req,res) => {
     res.render('index.ejs')
 })
+
+app.post('/shortUrls', (req,res) => {
+
+})
+
 
 app.listen(process.env.PORT || 3000);
