a1a54afc63ac1dbe4f6f34529d7798610f55cfef
Wasay Faizan <a.wasayfaizan@hotmail.com>
2022-07-27 17:18:49 +0500

    first commits

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+
+app.set('view engine', 'ejs')
+
+
+app.get('/', (req,res) => {
+    res.render('index.ejs')
+})
+
+app.listen(process.env.PORT || 3000);
