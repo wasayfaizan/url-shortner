abda24565b44dd692fa0d797786323cc2c2215d3
Wasay Faizan <a.wasayfaizan@hotmail.com>
2022-07-28 12:45:34 +0500

    First Commits

--- server.js
+++ server.js
@@ -1,22 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
 const app = express();
 
+// mongoose connection
 mongoose.connect('mongodb://localhost/urlshortner', {
     useNewUrlParser : true, 
     useUnifiedTopology : true
 })
 
 
+
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
 
 
-app.get('/', (req,res) => {
-    res.render('index.ejs')
+app.get('/', async (req,res) => {
+    const shortUrls = await ShortUrl.find();
+    res.render('index.ejs', {shortUrl : shortUrls})
 })
 
-app.post('/shortUrls', (req,res) => {
+app.post('/shortUrls', async (req,res) => {
+    await ShortUrl.create({
+        fullUrl : req.body.fullUrl
+    })
+    res.redirect('/')
+})
 
+app.get('/:shortUrl', async (req,res) => {
+   const shortUrl = await ShortUrl.findOne({shortUrl : req.params.shortUrl}) ;
+   if (shortUrl == null) {
+       return res.sendStatus(404);
+   } else {
+       shortUrl.NoOfClicks++;
+       shortUrl.save();
+       res.redirect(shortUrl.fullUrl)
+   }
 })
 
 
